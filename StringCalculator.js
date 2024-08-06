a5bba0fdb8b745eff1882db9ffb634d4f041bb08
rimpa92 <53218414+rimpa92@users.noreply.github.com>
2024-08-06 20:48:50 +0530

    Update StringCalculator.js
    Updated StringCalculator to accomodate condition of different delimiter

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,18 +1,16 @@
 function add (stringNum) {
-    let StringArr = stringNum.split(",");
 
-    let sum = 0;
+    let sum =0;
 
-    for (let i=0;i< StringArr.length;i++) {
-        sum = sum + parseInt(StringArr[i])
+    for (let i=0; i< stringNum.length;i++) {
+        if (!isNaN(parseInt(stringNum[i]))) {
+            sum = sum + parseInt(stringNum[i])
+        } 
+        
     }
 
-    if (isNaN(sum)) {
-        return 0
-    } else {
-         return sum;
-    }
+    return sum
 }
 
-var sum1 = add("1,5,7,8,9")
+var sum1 = add("//;\n1;2")
 console.log("The sum of numbers is",sum1)
