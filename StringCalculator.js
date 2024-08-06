4ef463fe7258a95e54af698c07e15a6afe8f19e4
rimpa92 <53218414+rimpa92@users.noreply.github.com>
2024-08-06 21:12:09 +0530

    Update StringCalculator.js
    Adding logic to accomodate negative number condition

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,15 +1,34 @@
 function add (stringNum) {
 
-    let sum =0;
+    let sum = 0;
+
+    let count = 0;
+    let negativeArray = []
+
+    
 
     for (let i=0; i< stringNum.length;i++) {
         if (!isNaN(parseInt(stringNum[i]))) {
-            sum = sum + parseInt(stringNum[i])
+            if (parseInt(stringNum[i]) < 0) {
+                count+=1;
+                negativeArray.push(parseInt(stringNum[i]))
+            } else {
+                sum = sum + parseInt(stringNum[i])
+            }
+           
         } 
         
     }
+    
+    if (count === 0) {
+        return sum;
+    } else if (count === 1) {
+        throw new Error(`negative numbers not allowed ${negativeArray[0]}`)
+     } else if (count > 1) {
+        throw new Error(`negative numbers not allowed ${[...negativeArray]}`)
+     }
 
-    return sum
+    
 }
 
 var sum1 = add("//;\n1;2")
